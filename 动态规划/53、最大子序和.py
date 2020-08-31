3f3fddf8d57eede9a79930d49d035845e9eb7212
tianmingbo <1414518976@qq.com>
2020-08-31 21:01:15 +0800

    '链表'

---
+++ 动态规划/53、最大子序和.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/31 17:27
+# @Author  : tmb
+
+class Solution:
+    def maxSubArray(self, nums):
+        if not nums:
+            return 0
+        dp = [0 for i in nums]
+        for i in range(len(nums)):
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+        return max(dp)
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
