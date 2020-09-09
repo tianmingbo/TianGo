b7b1a17a2816b493c68c08d455926e13fdd763c3
tianmingbo <1414518976@qq.com>
2020-09-09 21:12:31 +0800

    9.9

--- 分治&回溯/17、电话号码的字母组合.py
+++ 分治&回溯/17、电话号码的字母组合.py
@@ -18,7 +18,7 @@ class Solution:
 
         def helper(index):
             if index == len(digits):
-                res.append("".join(tmp))
+                res.append("".join(tmp))  # 不能append(tmp),浅拷贝，值会被覆盖
             else:
                 digit = digits[index]
                 for i in map[digit]:
@@ -31,7 +31,15 @@ class Solution:
         helper(0)
         return res
 
+    def letterCombinations2(self, digits):
+        map = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs",
+               '8': "tuv", '9': "wxyz"}
+        res = [''] if digits else []  # 为空返回[]
+        for q in digits:
+            res = [i + j for i in res for j in map[q]]  # niubility
+        return res
+
 
 if __name__ == '__main__':
     a = Solution()
-    print(a.letterCombinations('23'))
+    print(a.letterCombinations2('23'))
---
+++ 分治&回溯/51、N皇后.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/9 17:22
+# @Author  : tmb
+class Solution:
+    def solveNQueens(self, n: int):
+        xy_diff = set()
+        xy_add = set()
+
+
+if __name__ == '__main__':
+    a = Solution()
+    a.solveNQueens(8)
