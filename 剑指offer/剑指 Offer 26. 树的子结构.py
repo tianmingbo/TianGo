1fb8c6af7849f11168fc2575aa92e9000c2be015
tianmingbo <1414518976@qq.com>
2021-01-05 09:14:40 +0800

    1/5、日常

--- 剑指offer/剑指 Offer 26. 树的子结构.py
+++ 剑指offer/剑指 Offer 26. 树的子结构.py
@@ -14,3 +14,11 @@ class Solution:
         if not A or not B:
             return False
 
+        def helper(A, B):
+            if not B:
+                return True
+            if not A or A.val != B.val:
+                return False
+            return helper(A.left, B.left) and helper(A.right, B.right)
+
+        return helper(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
---
+++ 剑指offer/剑指 Offer 27. 二叉树的镜像.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/30 10:38
+# @Author  : tmb
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
