8cbdd0826215dd150d5dac237a27ce6beae9ecc3
tianmingbo <1414518976@qq.com>
2021-12-15 22:50:58 +0800

    qimingpian

--- test2.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
-}
-url = 'https://www.sohu.com'
-res = requests.get(url)
-print(res.cookies)
