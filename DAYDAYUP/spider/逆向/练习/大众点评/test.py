3330252f410da77ba801d0cef700797061bd7547
Tian <mingbotian@contractor.ea.com>
2022-01-12 14:00:44 +0800

    woff

--- DAYDAYUP/spider/逆向/练习/大众点评/test.py
+++ DAYDAYUP/spider/逆向/练习/大众点评/test.py
@@ -1,4 +1,9 @@
+import hashlib
+import re
+
 import requests
+from fontTools.ttLib import TTFont
+
 """
 1:根据首页中的css中，获取其中的woff字体文件。
 2：以其中的一个字体文件为例，建立映射关系
@@ -15,7 +20,7 @@ import requests
         <pt x="147" y="508" on="1"/>
         <pt x="154" y="580" on="0"/>
         <pt x="236" y="658" on="0"/>
-		……
+		...
         <pt x="418" y="374" on="1"/>
         <pt x="535" y="414" on="0"/>
         <pt x="535" y="534" on="1"/>
@@ -38,7 +43,7 @@ import requests
         <pt x="236" y="658" on="0"/>
         <pt x="308" y="657" on="1"/>
         <pt x="378" y="658" on="0"/>
-		……
+		...
         <pt x="481" y="355" on="0"/>
         <pt x="418" y="374" on="1"/>
         <pt x="535" y="414" on="0"/>
@@ -52,13 +57,43 @@ import requests
 	知道了这个方法，那么如何建立映射？
 	
 	可以以字形数据建立映射：{'name':['uniefba','uniecbf'],'value':'0','hex':md5(content)} #因为字形数据多，以MD5值来区分唯一值,
-	选择一个woff文件，……
-	
-
+	选择一个woff文件，
 """
-headers={
-'User-agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-}
 
-resp=requests.get('https://www.dianping.com/shop/G4mmqiZrzgkpQZGY',headers=headers)
-print(resp.text)
+# 访问页面需要验证，重点不在这里，直接拿到css文件，匹配里面的woff
+# headers = {
+#     'User-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+# }
+#
+# resp = requests.get('https://www.dianping.com/shop/G4mmqiZrzgkpQZGY', headers=headers)
+# print(resp.text)
+
+# with open('load.css', 'r') as f:
+#     css = f.read()
+# print(css)
+# woff_link = re.findall(re.compile(r',url\("(.*)\.woff"\);'), css)
+# woff_link = ['https:' + i + '.woff' for i in list(set(woff_link))]
+# for i in range(len(woff_link)):
+#     res = requests.get(woff_link[i])
+#     with open(f'{i}.woff', 'wb') as f:
+#         f.write(res.content)
+mapping = {}
+font = TTFont('1.woff')
+# font.saveXML('1.xml')
+# 提取字形信息
+# content = font['glyf'].glyphs
+# content = font.getGlyphNames()[1:-1]
+# with open('word.txt', 'r', encoding='utf-8') as f:
+#     word = f.read()
+# print(len(word))
+# print(len(content),content)
+# for i in range(len(content)):
+#     mapping.update({'name': content[i], 'value': word[i],
+#                     'hex': hashlib.md5(font['glyf'].glyphs.get(content[i]).data).hexdigest()})
+# print(mapping)
+# print(font.getGlyphID('unif4b0'))
+print(font.getGlyphOrder())
+print(len(font.getGlyphOrder()))
+#
+# for i in word:
+#     print(i)
