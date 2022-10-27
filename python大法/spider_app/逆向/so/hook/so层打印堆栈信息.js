2625d92314810244d8eff8c1452665624ee0901f
tianmingbo <1414518976@qq.com>
2022-10-27 22:23:50 +0800

    frida so api and frida 检测点

--- python大法/spider_app/逆向/so/hook/so层打印堆栈信息.js
+++ python大法/spider_app/逆向/so/hook/so层打印堆栈信息.js
@@ -1,2 +1,22 @@
 //so层打印函数栈
 console.log(Thread.backtrace(this.context, Backtracer.FUZZY).map(DebugSymbol.fromAddress).join('\n') + '\n');
+
+var soAddr = Module.findBaseAddress("libxiaojianbang.so");
+var updateAddr = soAddr.add(0x21B0);
+Interceptor.attach(updateAddr, {
+    onEnter: function (args) {
+        //console.log(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n');
+        console.log("==================================================");
+        console.log(soAddr);
+        console.log(Thread.backtrace(this.context, Backtracer.ACCURATE).map(function (value) {
+            var symbol = DebugSymbol.fromAddress(value);
+            console.log(symbol.moduleName);
+            if (symbol.moduleName === "libxiaojianbang.so") {
+                return symbol + " offset: " + value.sub(soAddr);
+            }
+            return symbol;
+        }).join('\n'));
+    }, onLeave: function (retval) {
+
+    }
+});
