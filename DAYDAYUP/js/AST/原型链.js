040c7441db701e6bafa5773eda6e8cb22e032f7d
tianmingbo <1414518976@qq.com>
2022-11-27 09:13:39 +0800

    AST

--- DAYDAYUP/js/AST/原型链.js
+++
@@ -1,9 +0,0 @@
-/**
-*每个函数都有prototype属性
- * 每个函数的prototype都指向实例的原型对象（空object）
- *
- * __proto__
- * 每个实例对象（除了null）都有__proto__
- * 该属性指向该对象的原型
-* */
-src/components/global-assign-hook-component/core/hook.js
--- python大法/spider/ast/ast.md
+++
@@ -1,2 +0,0 @@
-1、ast：https://github.com/NightTeam/JavaScriptAST
-2、
