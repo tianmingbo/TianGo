568b74f60b6aa98f5e60ec584b2fd1f78b0408c4
mingbo.tian <mingbo.tian@westwell-lab.com>
2023-08-04 13:33:33 +0800

    redis util

--- DAYDAYUP/数据库/redis/redis/src/util.c
+++ DAYDAYUP/数据库/redis/redis/src/util.c
@@ -602,13 +602,7 @@ void getRandomHexChars(char *p, size_t len) {
     for (j = 0; j < len; j++) p[j] = charset[p[j] & 0x0F];
 }
 
-/* Given the filename, return the absolute path as an SDS string, or NULL
- * if it fails for some reason. Note that "filename" may be an absolute path
- * already, this will be detected and handled correctly.
- *
- * The function does not try to normalize everything, but only the obvious
- * case of one or more "../" appearing at the start of "filename"
- * relative path. */
+/* 以 SDS 字符串形式返回绝对路径 */
 sds getAbsolutePath(char *filename) {
     char cwd[1024];
     sds abspath;
@@ -617,7 +611,7 @@ sds getAbsolutePath(char *filename) {
     relpath = sdstrim(relpath, " \r\n\t");
     if (relpath[0] == '/') return relpath; /* Path is already absolute. */
 
-    /* If path is relative, join cwd and relative path. */
+    /* 获取当前工作目录 */
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
         sdsfree(relpath);
         return NULL;
@@ -626,12 +620,7 @@ sds getAbsolutePath(char *filename) {
     if (sdslen(abspath) && abspath[sdslen(abspath) - 1] != '/')
         abspath = sdscat(abspath, "/");
 
-    /* At this point we have the current path always ending with "/", and
-     * the trimmed relative path. Try to normalize the obvious case of
-     * trailing ../ elements at the start of the path.
-     *
-     * For every "../" we find in the filename, we remove it and also remove
-     * the last element of the cwd, unless the current cwd is "/". */
+    /*  去除relpath中开头的'../' */
     while (sdslen(relpath) >= 3 &&
            relpath[0] == '.' && relpath[1] == '.' && relpath[2] == '/') {
         sdsrange(relpath, 3, -1);
@@ -647,7 +636,7 @@ sds getAbsolutePath(char *filename) {
         }
     }
 
-    /* Finally glue the two parts together. */
+    /* 拼接两部分得到绝对路径*/
     abspath = sdscatsds(abspath, relpath);
     sdsfree(relpath);
     return abspath;
