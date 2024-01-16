d1b70ea632e872b1dcc16e8dc24f90bef353e697
tianmingbo <1414518976@qq.com>
2024-01-16 22:08:01 +0800

    动态页面缓存

--- python大法/aio/daliApi/app/controllers/default.py
+++ python大法/aio/daliApi/app/controllers/default.py
@@ -5,14 +5,17 @@
 @time: 2024/1/15  20:40
 @desc: 
 """
+from dali.cache import cache
 from dali.http import Response, Request
 
 
+@cache(expire=5)
 async def index(req: Request, res: Response):
     await res.end(b'default page')
 
 
 class Hello:
     # 接受调用
+    @cache()
     async def __call__(self, req: Request, res: Response):
         await res.end(b'default page')
