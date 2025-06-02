8cc85064ef256300bbab21c328acff7245ae6b76
mingbo.tian <mingbotian@hotmail.com>
2025-06-03 07:37:10 +0800

    [go] metadata and interceptor

--- Language/go/rpc/grpc/metadata/client.go
+++ Language/go/rpc/grpc/metadata/client.go
@@ -5,26 +5,55 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"lGo/rpc/grpc/hello/proto/pb"
+	"google.golang.org/grpc/metadata"
 	"lGo/rpc/grpc/metadata/pb"
 	"log"
 	"time"
 )
 
+// 客户端全局拦截器，统一添加metadata
+func clientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// 添加全局元数据（如版本号、时间戳）
+	fmt.Println("method: ", method)
+	md := metadata.Pairs(
+		"client-version", "v1.0.0",
+		"request-time", time.Now().Format(time.RFC3339),
+	)
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	// 调用原始方法
+	return invoker(ctx, method, req, reply, cc, opts...)
+}
+
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(clientInterceptor))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+	//构建metadata
+	//md := metadata.Pairs(
+	//	"User-agent", "grpc",
+	//	"authorization", "Bearer your-token-123",
+	//	"client-type", "web")
+	md := metadata.New(map[string]string{
+		"authorization": "Bearer your-token-123",
+		"client-type":   "web"})
+
 	c := pb.NewHelloServiceClient(conn) //  创建客户端
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	res, err := c.SayHello(ctx, &pb2.HelloRequest{Name: "Tian"}) //  调用 SayHello 方法
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	var headerMD, trailerMD metadata.MD
+	res, err := c.SayHello(ctx, &pb.HelloRequest{Name: "Tian"},
+		grpc.Header(&headerMD),
+		grpc.Trailer(&trailerMD)) //  调用 SayHello 方法
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
 	fmt.Println("Response from server:", res.Message)
+	// 处理头元数据
+	log.Printf("响应头: %v", headerMD)
+	// 处理尾随元数据
+	log.Printf("尾随元数据: %v", trailerMD)
 }
--- Language/go/rpc/grpc/metadata/server.go
+++ Language/go/rpc/grpc/metadata/server.go
@@ -3,28 +3,63 @@ package main
 import (
 	"context"
 	"fmt"
-	"google.golang.org/grpc"
-	pb2 "lGo/rpc/grpc/hello/proto/pb"
 	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"lGo/rpc/grpc/metadata/pb"
 )
 
-type Server struct {
-	pb2.UnimplementedHelloServiceServer //把接口里面的方法全部实现一遍
+// 服务端拦截器（统一验证元数据）
+func serverInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	//metadata解析
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+	fmt.Println("metadata: ", md) //val为[]string.  slice类型
+	if auth, ok := md["authorization"]; ok {
+		token := auth[0]
+		if token != "Bearer your-token-123" {
+			return nil, status.Errorf(codes.Unauthenticated, "token is invalid")
+		}
+		fmt.Println("token: ", token)
+	}
+	//创建响应元数据（头）
+	headersMetadata := metadata.Pairs("x-response-code", "200")
+	if err := grpc.SendHeader(ctx, headersMetadata); err != nil {
+		return nil, err
+	}
+	// 创建尾随元数据（请求结束时返回）
+	trailerMD := metadata.Pairs(
+		"x-total-count", "100", // 统计信息
+	)
+	err := grpc.SetTrailer(ctx, trailerMD)
+	if err != nil {
+		return nil, err
+	}
+	return handler(ctx, req)
 }
 
-func (s *Server) SayHello(ctx context.Context, req *pb2.HelloRequest) (*pb2.HelloReply, error) {
-	return &pb2.HelloReply{Message: "Hello, " + req.Name}, nil
+type Server struct {
+	pb.UnimplementedHelloServiceServer //把接口里面的方法全部实现一遍
+}
+
+func (s *Server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello, " + req.Name}, nil
 }
 
 func main() {
-	listen, err := net.Listen("tcp", ":50051") //监听端口
+	listen, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		fmt.Println("failed to listen:", err)
 	}
-	s := grpc.NewServer()                        //创建grpc服务
-	pb2.RegisterHelloServiceServer(s, &Server{}) //注册服务
+	s := grpc.NewServer(grpc.UnaryInterceptor(serverInterceptor)) //使用拦截器
+	pb.RegisterHelloServiceServer(s, &Server{})
 	fmt.Println("gRPC server listening on :50051")
-	if err := s.Serve(listen); err != nil { //启动服务
+	if err := s.Serve(listen); err != nil {
 		fmt.Println("failed to serve:", err)
 	}
 }
