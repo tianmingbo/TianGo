dd3013bacf9efaaa54e1e5080e7a671dc3547ec3
mingbotian <1414518976@qq.com>
2025-07-21 19:01:06 +0800

    [MQ] rmq select queue

--- Foundation/MQ/rocketmq/producer/orderly/main.go
+++ Foundation/MQ/rocketmq/producer/orderly/main.go
@@ -11,24 +11,38 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/producer"
 )
 
+type myManualQueueSelector struct{}
+
+func newMyQueueSelector() producer.QueueSelector {
+	return new(myManualQueueSelector)
+}
+
+func (myManualQueueSelector) Select(message *primitive.Message, queues []*primitive.MessageQueue, lastBrokerName string) *primitive.MessageQueue {
+	message.Queue.BrokerName = queues[0].BrokerName
+	fmt.Println("指定queueId=", message.Queue.QueueId)
+	return message.Queue
+}
+
 // Package main implements a simple producer to send message.
 func main() {
 	p, _ := rocketmq.NewProducer(
 		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{"10.6.64.191:9876"})),
 		producer.WithRetry(2),
-		producer.WithTls(true),
+		producer.WithQueueSelector(newMyQueueSelector()),
 	)
+
 	err := p.Start()
 	if err != nil {
 		fmt.Printf("start producer error: %s", err.Error())
 		os.Exit(1)
 	}
 	topic := "test"
-
-	for i := 0; i < 10; i++ {
+	//指定queueId
+	for i := 0; i < 100; i++ {
 		msg := &primitive.Message{
 			Topic: topic,
 			Body:  []byte("Hello RocketMQ Go Client! " + strconv.Itoa(i)),
+			Queue: &primitive.MessageQueue{Topic: topic, QueueId: 1},
 		}
 		res, err := p.SendSync(context.Background(), msg)
 
